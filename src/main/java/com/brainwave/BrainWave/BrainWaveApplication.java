2f236d0473dbb1908bee5f948e3c7baa13da85e3
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2024-09-01 10:01:20 +0530

    setup brainwave

---
+++ src/main/java/com/brainwave/BrainWave/BrainWaveApplication.java
@@ -0,0 +1,13 @@
+package com.brainwave.BrainWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brainwave/BrainWave/BrainWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainwave.BrainWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
